ad51e28d18881450ad729d0e442e7aa553a16ba5
diegoG82 <diegogiardina@hotmail.it>
2023-03-07 16:59:05 +0100

    formattazione script js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
 // TEST JS
 // alert("CIAO!")
 
-
 // ESERCIZIO DA SVOLGERE:
 // Chiedi all’utente il suo nome,
 // poi chiedi il suo cognome,
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
-
 // RACCOLTA DATI:
 
 // Chiedere il nome all'utente:
 const username = prompt("Come ti chiami?");
 // inserire console log con verifica del tipo di dato acquisito
-console.log(username, typeof username );
+console.log(username, typeof username);
 
 // chiedere il cognome all'utente:
-const surname = prompt("Qual'e'il tuo cognome?")
+const surname = prompt("Qual'e'il tuo cognome?");
 // inserire console log con verifica del tipo di dato acquisito
 console.log(surname, typeof surname);
 
 // Chiedere all'utente qual'e' il suo colore preferito
-const favouritecolor = prompt("Qual'è il tuo colore preferito?")
+const favouritecolor = prompt("Qual'è il tuo colore preferito?");
 // inserire console log con verifica del tipo di dato acquisito
 console.log(favouritecolor, typeof favouritecolor);
-
 
 // Composizione della password combinando i dati
 const message = `${username}${surname}${favouritecolor}21`;
@@ -33,6 +30,3 @@ console.log(message);
 
 // OUTPUT DEI DATI
 document.getElementById("insicurissima").innerHTML = message;
-
-
-
