cb584c8f64c368ce5fe812827587e8dfab503600
diegoG82 <diegogiardina@hotmail.it>
2023-03-07 16:01:18 +0100

    Inserito script js piu' custom style

--- js/script.js
+++ js/script.js
@@ -9,5 +9,30 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 
+// RACCOLTA DATI:
+
+// Chiedere il nome all'utente:
+const username = prompt("Come ti chiami?");
+// inserire console log con verifica del tipo di dato acquisito
+console.log(username, typeof username );
+
+// chiedere il cognome all'utente:
+const surname = prompt("Qual'e'il tuo cognome?")
+// inserire console log con verifica del tipo di dato acquisito
+console.log(surname, typeof surname);
+
+// Chiedere all'utente qual'e' il suo colore preferito
+const favouritecolor = prompt("Qual'è il tuo colore preferito?")
+// inserire console log con verifica del tipo di dato acquisito
+console.log(favouritecolor, typeof favouritecolor);
+
+
+// Composizione della password combinando i dati
+const message = `${username}${surname}${favouritecolor}21`;
+console.log(message);
+
+// OUTPUT DEI DATI
+document.getElementById("insicurissima").innerHTML = message;
+
 
 
